a61faf9a765752f35af650b722370154e4ab3f28
Abdul W <wahibabdulrahman@gmail.com>
2023-12-06 18:45:21 -0500

    change from MongoDB to PostgreSQL

--- app.js
+++ app.js
@@ -1,101 +1,103 @@
 //jshint esversion:6
 
-import express from "express";
-import bodyParser from "body-parser"; 
-import _ from "lodash";
-import mongoose from "mongoose";
+import express from 'express';
+import bodyParser from 'body-parser';
+import _ from 'lodash';
+import pg from 'pg';
+const { Pool } = pg;
 import 'dotenv/config'
-import {Pool, Client} from "pg";
 
+const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/blogPostsDB');
-
-const postSchema = new mongoose.Schema (
-  {
-    title: String,
-    content: String,
-  }
-);
-
-const Post = mongoose.model("Post", postSchema);
+const itemsPool = new Pool({
+    connectionString: process.env.DBCONLINK,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-const app = express();
-
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.get('/', async (req, res) => {
+    const client = await itemsPool.connect();
 
-app.get("/", function(req, res){
-    Post.find({}).then( function(posts) {
-      if (posts.length < 1) {
-        const defaultPost = new Post (
-          {
-            title: "Example post",
-            content: "This is an example blog post for test purposes.",
-          }
-        );
-        defaultPost.save();
-        res.render("home", {
-          startingContent: homeStartingContent,
-          posts: posts
-        });
-      } else {
-        res.render("home", {
-          startingContent: homeStartingContent,
-          posts: posts
-        });
-      };
-    });
-  });
+    try {
+        const result = await client.query('SELECT title, content FROM posts');
+        const posts = result.rows;
 
-
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+        if (posts.length < 1) {
+            const defaultPost = {
+                title: 'Example post',
+                content: 'This is an example blog post for test purposes.',
+            };
+            res.render('home', { startingContent: homeStartingContent, posts: [defaultPost] });
+        } else {
+            res.render('home', { startingContent: homeStartingContent, posts: posts });
+        }
+    } catch (err) {
+        console.error(err);
+    } finally {
+        client.release();
+    }
 });
 
+app.get("/about", function(req, res){
+    res.render("about", {aboutContent: aboutContent});
+  });
+  
 app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+res.render("contact", {contactContent: contactContent});
 });
 
 app.get("/compose", function(req, res){
-  res.render("compose");
+res.render("compose");
 });
 
-app.post("/compose", function(req, res){
-  const post = new Post ({
+app.post("/compose", async (req, res) => {
+    const client = await itemsPool.connect();
 
-    title: req.body.postTitle,
-    content: req.body.postBody
- 
-  });
-
-  post.save();
-  res.redirect("/");
-
-  });
-
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  Post.find({}).then( function(posts) {
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
+    try {
+        const { postTitle, postBody } = req.body;
+        await client.query('INSERT INTO posts (title, content) VALUES ($1, $2)', [postTitle, postBody]);
+        res.redirect("/");
+    } catch (err) {
+        console.error(err);
+    } finally {
+        client.release();
     }
-  });
-  });
+});
+
+app.get("/posts/:postName", async (req, res) => {
+    const requestedTitle = _.lowerCase(req.params.postName);
+
+    const client = await itemsPool.connect();
+
+    try {
+        const result = await client.query('SELECT title, content FROM posts');
+        const posts = result.rows;
+
+        posts.forEach(post => {
+            const storedTitle = _.lowerCase(post.title);
+
+            if (storedTitle === requestedTitle) {
+                res.render("post", {
+                    title: post.title,
+                    content: post.content
+                });
+            }
+        });
+    } catch (err) {
+        console.error(err);
+    } finally {
+        client.release();
+    }
 });
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
